6baddfe1f2f2a749a07dc45aa490e104f408de1c
trantrang126 <86888545+trantrang126@users.noreply.github.com>
2021-07-04 00:21:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Người yêu Trang ơiiiii!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Trang <33',
+    btnIntro: 'hihi ❤',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên ❣',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy 💞 ',
+    btnYes: 'Vẫn cứ là yêu Trang 💖❤',
     btnNo: 'Không, em à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì em ngon :))))) 🤣🤣🤣',
     mess: 'em biết mà 🥰. Yêu Toàn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h em qua chơi với anh nhá .',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Tối nay 7h em qua chơi với anh nhá 😘🥰😘 .',
+    btnAccept: 'Okiiiii lun <3, cho anh ghi sổ nợ 200k nữa nhá =))))',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
