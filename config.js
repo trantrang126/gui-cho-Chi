c0a32ed2149a4ce0ab02cbd40a09b71eb5c752ee
trantrang126 <86888545+trantrang126@users.noreply.github.com>
2022-02-24 16:18:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Người yêu Trang ơiiiii!',
+    introTitle: 'Anh Dũng ơiiiii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi ❤',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên ❣',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy 💞 ',
-    btnYes: 'Vẫn cứ là yêu Trang 💖❤',
-    btnNo: 'Không, em à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnYes: 'Vẫn cứ là yêu Trang 💖',
+    btnNo: 'Không, em tuổi 😏:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại dính chân vào em vậy???? <3',
     btnReply: 'Gửi cho em <3',
-    reply: 'Vì em ngon 🤣🤣🤣 :)))',
-    mess: 'em biết mà 🥰. Yêu anh Vũ nhiều nhiều ạ 😘😘',
-    messDesc: '31/12 anh tỏ tình em nhớ chưa, ko được quên đâu, quên là em dỗi 5' đấy :))) 😘�💞 .',
-    btnAccept: 'Okiiiii lun <3, vẫn kèo 12/6 đấy nhá 🤣🤣🤣 =))))',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Vì em là Ran còn anh là Shinichi, à vì em ngon nữa 🤣🤣🤣 :)))',
+    mess: 'em biết mà 🥰. Yêu anh Dũng nhiều nhiều ạ 😘😘',
+    messDesc: 'Bao h đi công tác về tỏ tình em hẳn hoi nhớ chưa, ko được quên đâu, quên là em dỗi 5' đấy :))) 😘�💞 .',
+    btnAccept: 'Okiiiii lun <3, đợi anh nhé 🥰 =))))',
+    messLink: 'https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fview%2Fkiss-love-lovers-cute-bear-gif-16293392&psig=AOvVaw3MxpNHX93D3K1xCmCHZICO&ust=1645780707484000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDam4aBmPYCFQAAAAAdAAAAABAD' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
